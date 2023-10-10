dabf4fadda4dde4255472ee6a1e46792715b8174
Cristian Ionel <cristian.ionel@monster.com>
2023-10-10 16:05:26 +0200

    Almost ready

--- resources/js/components/PreviewIframe.vue
+++ resources/js/components/PreviewIframe.vue
@@ -16,7 +16,7 @@
                 <base target='_blank' />
                 <link rel='preconnect' href='https://fonts.googleapis.com'>
                 <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
-                <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&family=Anton:wght@400&display=swap' rel='stylesheet'>
+                <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&family=Oswald:wght@400;600;700&family=Archivo:wght@400;600&display=swap' rel='stylesheet'>
                 <link rel='stylesheet' href='${stylesheet}' />
                 <script type='module'>
                 window.addEventListener('message', (event) => {

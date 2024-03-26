9d0aae67f7dc24d9a7ff3d670de414fcceb7860d
Cristian Ionel <cristian.ionel@monster.com>
2024-03-26 19:08:25 +0100

    Preview button for mobile

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -10,9 +10,18 @@
         class="relative"
         @keyup.escape="selectGroup(null)">
         <template #field>
-            <div class="p-3 text-right top-preview-buttons d-none" v-if="currentField.enablePreview">
+            <div class="p-3 text-right top-preview-buttons flex justify-end d-none" v-if="currentField.enablePreview">
                 <a v-if="!fullScreen" class="shadow relative text-white cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 inline-flex items-center justify-center h-9 px-4 ml-2 change-status" @click="changeStatus">Change Status</a>
-                <a v-if="!fullScreen" class="shadow relative text-white cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 inline-flex items-center justify-center h-9 px-4 ml-2 exit-fullscreen" @click="visitSite">Visit Site</a>
+                <a v-if="!fullScreen" class="shadow relative bg-primary-900 hover:bg-primary-900 text-white cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 inline-flex items-center justify-center h-9 px-4 ml-2"
+                   @click="mobileView = !mobileView"
+                   :title="mobileView ? 'Switch to Desktop View' : 'Switch to Mobile View'"
+                ><svg fill="transparent" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="20" height="20">
+                    <path v-if="!mobileView" stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"></path>
+                    <path v-if="mobileView" stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"></path>
+                </svg>
+                </a>
+                <a v-if="!fullScreen" class="shadow relative text-white cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 inline-flex items-center justify-center h-9 px-4 ml-2 exit-fullscreen" @click="visitSite">Preview</a>
+
                 <LoadingButton
                     v-if="!fullScreen"
                     dusk="update-button"
@@ -60,7 +69,7 @@
                     </a>
 
 
-                    <a class="shadow relative text-white cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 inline-flex items-center justify-center h-9 px-4 ml-2 exit-fullscreen" @click="visitSite">Visit Site</a>
+                    <a class="shadow relative text-white cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 inline-flex items-center justify-center h-9 px-4 ml-2 exit-fullscreen" @click="visitSite">Preview</a>
 
                     <LoadingButton
                         dusk="update-button"
